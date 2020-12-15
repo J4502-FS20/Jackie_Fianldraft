7c1268e3794ab4e68930abcccd569f38ff57b6b4
Jackie-Lai27 <jlx4y@umsystem.edu>
2020-12-14 19:52:05 -0600

    minor update

--- selectbuttom.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var dropdown = document.getElementById('dropdownMenu');
-
-dropdown.addEventListener('change', function() {
-  var selectedSection = document.getElementById(dropdown.value);
-  selectedSection.scrollIntoView({behavior: "smooth"});
-});
